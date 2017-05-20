0004bfccb01241035af4c9b24792e12715e96950
Doğa Barış <dogabarisozdemir@gmail.com>
2017-05-20 23:15:59 +0600

    minor

--- config/cron.js
+++ config/cron.js
@@ -1,28 +1,25 @@
 module.exports.cron = {
 
   VeriUret: {
-    //schedule: '*/10 * * * * *', //her 10 sn'de bir çalışır.
-    schedule: '00 28 19 * * *', //her gün saat 10:00:00'da yapay zeka scripti çalıştırılarak sonuç üretilir ve apilere yollanır.
+    schedule: '*/30 * * * * *', //her 10 sn'de bir çalışır.
+    //schedule: '00 36 21 * * *', //her gün saat 10:00:00'da yapay zeka scripti çalıştırılarak sonuç üretilir ve apilere yollanır.
     onTick: function () {
       console.log("tick");
       var fs = require('fs');
       var request = require('request');
       var Twitter = require('twitter');
-      var GoogleUrl = require( 'google-url' );
+      var GoogleUrl = require('google-url');
       googleUrl = new GoogleUrl( { key: 'AIzaSyCANvOYmqH9-4a0e39Pe3xx99WSIAUyeDc' });
-
-
       var client = new Twitter({//twitter app bilgileri.
         consumer_key: 'bL7eaEI0w3EhJbuM1rtUXrNjs',
         consumer_secret: 'pDc7R0iSNX2Nh8mIJ93p4xKcPEiA0cIbNdwtAJVunGcDP8Iru4',
-        access_token_key: '865331824108728320-865331824108728320-bPe2vX1qWBcRqJICpubS0mtNrlQfwrK',
-        access_token_secret: 'INPo67sgsbaWhQDehWSjDp2grCJTsxRJPTyVCWxQr0JC8'
+        access_token_key: '865331824108728320-fldTUJSsBmBIEEbc7yVl3B1eSbrRePc',
+        access_token_secret: 'j5scoCfEy4IsEu0XUH5T2CvLjWrB7wJQR2zMv4E2xVh29'
       });
-
+      var token = "EAACl9XZC0IeYBALzrIycNGBUZAP4qwuTVzQ0MAgCU1gTOtD8ihQZAESTj8JRz7sfTn453ZBZBpaHTAzg6GbhXZANHfwVGZAqV70e022v8kZAZBDdL6YrGijlZAbYQf1fvAl2ZBuu4zR7jc8JHVOCezHifDTelZAlYKUVs7AZD"
+      //longlivetoken
 
       if (fs.existsSync('k-means.R')) {
-          console.log("var ");
-
           var sys = require('sys')
           var exec = require('child_process').exec;
           function puts(error, stdout, stderr) {
@@ -31,39 +28,39 @@ module.exports.cron = {
           exec("Rscript D:/gunesPaneli/k-means.R", puts);
 
           fs.readFile("sonuc.txt", 'utf8', function(err, data) {
-            if (err) throw err;
+            if (err){ console.log(err);}
             console.log(data);//çıkarım sonucu
-            googleUrl.shorten( sails.getBaseUrl(), function( err, shortUrl ) {
-                // shortUrl should be http://goo.gl/BzpZ54
+
+            googleUrl.shorten( sails.getBaseUrl(), function( err, shortUrl ) {//rul kısaltılıyor.
                 console.log(shortUrl);
 
+              	request({
+              		url: "https://graph.facebook.com/v2.8/1871951589723709/feed",
+              		qs: { access_token: token },
+              		method: "POST",
+              		json: {
+              			//url: "https://0785d3cb.ngrok.io",
+                    message: data + " -> " + shortUrl,
+              		}
+              	}, function(error, response, body) {
+                  if (error) {
+                    console.log("Error sending message: ", error);
+                  } else if (response.body.error) {
+                    console.log("Facebook Error: ", response.body.error);
+                  }else {
+                    console.log("Facebook'ta paylaşıldı.");
+                  }
+              	});
 
-          	request({//facebook sayfasında paylaşılıyor.
-          		url: "https://graph.facebook.com/v2.8/1871951589723709/feed",
-          		qs: { access_token: "EAACl9XZC0IeYBAH3k0rHIYq3ER8WLiQb9GkKZBUnmGfq0tbZCZCcCFWkSNZCadofrZCRsojq8Rp9Q5ZAsMgoTqtWfXxjNh2apAXsIqQUYTr8lKlywXwhLwYWhZBt4UeehxvTRB54ZC4AnypSBeZBEGQqkLj2Bq4moS9Gdtf08w3To0IEZAlOuZCA09ZAiqPZBSuiOc1vYZD" },
-          		method: "POST",
-          		json: {
-          			//url: "http://95fc700d.ngrok.io",
-                message: data + " -> " + shortUrl
-          		}
-          	}, function(error, response, body) {
-          		if (error) {
-          			console.log("Error sending message: ", error);
-          		} else if (response.body.error) {
-          			console.log("Error: ", response.body.error);
-              }else {
-                console.log("facebook'ta paylaşıldı.");
-              }
-        	  });
 
             var status = {
-                status: data + " " + shortUrl
+                status: data + " " + shortUrl,
             }
             client.post('statuses/update', status, function(error, tweet, response) {
                 if (!error) {
-                  console.log("tweet atıldı.");
+                  console.log("Tweet atıldı.");
                 }else {
-                  console.log(error + " " + response);
+                  console.log("Twitter Error: ", response.body.error);
                 }
             });
 
@@ -84,7 +81,7 @@ module.exports.cron = {
 
 
       }else{
-          console.log("yok");
+          console.log("R scripti bulunamadı.");
       }
 
     }
